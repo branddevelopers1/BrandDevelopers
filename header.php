efb8554bab2715bc9b6ffd1282b070b8bb8cf4df
branddevelopers1 <info@branddevelopers.ca>
2026-02-02 18:14:09 -0500

    tranparent background

--- header.php
+++ header.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brand Developers | We Build Brands That Work. </title>
-
-
     
     <?php wp_head();?>
 </head>
